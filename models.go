eb84bed4c9bd24b5466665b3cab80d702dc7320d
Henglongung <unghenglong567@gmail.com>
2021-09-23 15:04:27 +0700

    porm-3:Migrate Pingorm sample database entities

--- models.go
+++ models.go
@@ -1,10 +1,33 @@
 package pingorm
 
+import "time"
+
 // Model sample entities here
 type (
-	TestTable struct {
-		ID     uint32 `gorm:"primaryKey"`
-		Field2 string
+	Author struct {
+		ID         uint32 `gorm:"primaryKey"`
+		AuthorName string `json:"authorName"`
+		Age        uint32 `json:"age"`
+	}
+)
+
+type (
+	Editor struct {
+		ID         uint32 `gorm:"primaryKey"`
+		EditorName string `json:"editorName"`
+		Age        uint32 `json:"age"`
+	}
+)
+
+type (
+	Book struct {
+		ID          uint32 `gorm:"primaryKey"`
+		Title       string `json:"title"`
+		PublishDate time.Time
+		AuthorID    uint32
+		EditorID    uint32
+		Author      Author `gorm:"foreignKey:AuthorID;<-:false"`
+		Editor      Editor `gorm:"foreignKey:EditorID;<-:false"`
 	}
 )
 
